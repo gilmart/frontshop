a86be17c672f54ee34ab8db64c07848fbfd44e5f
“amonsalv” <anamonsalve96@gmail.com>
2023-05-27 10:23:32 -0500

    avance front

--- assets/components/Customer.js
+++ assets/components/Customer.js
@@ -1,58 +1,115 @@
-import {  Text, View } from 'react-native';
-import { TextInput, Button } from 'react-native-paper';
-import {useForm , Controller} from 'react-hook-form';
-import { useState } from 'react';
-import { styles } from '../styles/styles'
-import axios from 'axios';
+import { Text, View } from "react-native";
+import { TextInput, Button } from "react-native-paper";
+import { useForm, Controller } from "react-hook-form";
+import { useState } from "react";
+import { styles } from "../styles/styles";
+import axios from "axios";
 
 export default function Customer() {
-  const [isError, setIsError] = useState(false)
-  const [message, setMessage] = useState('')
-  const [idSearch, setIdSearch] = useState('')
+  const [isError, setIsError] = useState(false);
+  const [message, setMessage] = useState("");
+  const [idSearch, setIdSearch] = useState("");
 
-  const { control, handleSubmit, formState: { errors }, reset } = useForm({
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+    reset,
+    setValue,
+  } = useForm({
     defaultValues: {
-      firstName: '',
-      lastName: ''
-    }
+      firstName: "",
+      lastName: "",
+    },
   });
   const onSave = async (data) => {
     let nombre = data.firstName;
-    let apellidos = data.lastName
-    const response = await axios.post(`http://127.0.0.1:3000/api/clientes` ,{
+    let apellidos = data.lastName;
+    const response = await axios.post(`http://127.0.0.1:3000/api/clientes`, {
       nombre,
       apellidos,
-    })
+    });
     setIsError(false);
-    setMessage("cliente agregado correctamente... ")
-    setTimeout(()=>{
-      setMessage("")
-    },2000)
-    reset()
+    setMessage("cliente agregado correctamente... ");
+    setTimeout(() => {
+      setMessage("");
+    }, 2000);
+    reset();
+  };
 
+  const onSearch = async () => {
+    const response = await axios.get(
+      `http://127.0.0.1:3000/api/clientes/${idSearch}`
+    );
+    if (!response.data.error) {
+      //si encuentra el id search
+      setValue("firstName", response.data.nombre);
+      setValue("lastName", response.data.apellidos);
+      setMessage("");
+      setIsError(false);
+    } else {
+      setIsError(true);
+      setMessage("ID no encontrado ");
+    }
+
+    console.log(response.data);
+  };
+
+  const onUpdate = async (data) => {
+    const response = await axios.put(
+      `http://127.0.0.1:3000/api/clientes/${idSearch}`,
+      {
+        nombre: data.firstName,
+        apellidos: data.lastName,
+      }
+    );
+    setIsError(false);
+    setMessage("cliente actualizado correctamente... ");
+    setTimeout(() => {
+      setMessage("");
+    }, 2000);
+    reset();
+  };
+
+  const onDelete = async (data) => {
+    if (
+      confirm(
+        `esta seguro de eliminar al cliente... ${data.firstName} ${data.lastName}?`
+      )
+    ) {
+      const response = await axios.delete(
+        `http://127.0.0.1:3000/api/clientes/${idSearch}`
+      );
+
+      setIsError(false);
+      setMessage("cliente eliminado correctamente... ");
+      setTimeout(() => {
+        setMessage("");
+      }, 2000);
+      reset();
+    }
   };
 
   return (
     <View style={styles.container}>
       <Text>Actualizacion de clientes</Text>
       <TextInput
-        style={{marginTop:5, marginBottom:5}}
+        style={{ marginTop: 5, marginBottom: 5 }}
         label="id del cliente a buscar"
         mode="outlined"
         value={idSearch}
-        onChangeText={idSearch => setIdSearch(idSearch)}
-
+        onChangeText={(idSearch) => setIdSearch(idSearch)}
       />
       <Controller
         control={control}
         rules={{
-         required: true,
+          required: true,
         }}
         render={({ field: { onChange, onBlur, value } }) => (
           <TextInput
             label="Nombre Completo"
-            mode='outlined'
-            style={{backgroundColor:'powderblue'}}
+            mode="outlined"
+            style={{ backgroundColor: "powderblue" }}
             onBlur={onBlur}
             onChangeText={onChange}
             value={value}
@@ -60,18 +117,20 @@ export default function Customer() {
         )}
         name="firstName"
       />
-      {errors.firstName && <Text style={{color:'red'}}>El nombre es obligatorio.</Text>}
+      {errors.firstName && (
+        <Text style={{ color: "red" }}>El nombre es obligatorio.</Text>
+      )}
 
       <Controller
         control={control}
         rules={{
-         required: true,
+          required: true,
         }}
         render={({ field: { onChange, onBlur, value } }) => (
           <TextInput
             label="Apellidos"
-            mode='outlined'
-            style={{backgroundColor:'gray',marginTop:'10'}}
+            mode="outlined"
+            style={{ backgroundColor: "gray", marginTop: "10" }}
             onBlur={onBlur}
             onChangeText={onChange}
             value={value}
@@ -79,43 +138,49 @@ export default function Customer() {
         )}
         name="lastName"
       />
-      {errors.lastName && <Text style={{color:'red'}}>El apellido es obligatorio.</Text>}
-      <Text style={{color: isError ? 'red' : 'green'}}>{message}</Text>    
-      
+      {errors.lastName && (
+        <Text style={{ color: "red" }}>El apellido es obligatorio.</Text>
+      )}
+      <Text style={{ color: isError ? "red" : "green" }}>{message}</Text>
 
-      <View style={{marginTop:20, flexDirection:'row'}}>
-          <Button
-            icon="content-save"
-            mode="contained"
-            onPress={handleSubmit(onSave)}>  
+      <View style={{ marginTop: 20, flexDirection: "row" }}>
+        <Button
+          icon="content-save"
+          mode="contained"
+          onPress={handleSubmit(onSave)}
+        >
           Guardar
-          </Button>
+        </Button>
 
-
-          <Button
-            style={{backgroundColor:'orange', marginLeft:10}}
-            icon="content-save"
-            mode="contained"
-            //onPress={onSearch}
-          >  
+        <Button
+          style={{ backgroundColor: "orange", marginLeft: 10 }}
+          icon="content-save"
+          mode="contained"
+          onPress={onSearch}
+        >
           Buscar
-          </Button>
+        </Button>
       </View>
-      
-      
-      <View style={{marginTop:20, flexDirection:'row'}}>
-        <Button 
-          icon="pencil-outline" 
-          mode="contained" onPress={() => console.log('Pressed')}>
+
+      <View style={{ marginTop: 20, flexDirection: "row" }}>
+        <Button
+          icon="pencil-outline"
+          mode="contained"
+          onPress={handleSubmit(onUpdate)}
+        >
           Actualizar
         </Button>
-        <Button 
-          style={{backgroundColor:'red',marginLeft:10}}
-          icon="delete-outline" 
-          mode="contained" onPress={() => console.log('Pressed')}>
+
+        <Button
+          style={{ backgroundColor: "red", marginLeft: 10 }}
+          icon="delete-outline"
+          mode="contained"
+          onPress={handleSubmit(onDelete)}
+        >
           Eliminar
         </Button>
       </View>
+
       {/*
       <View style={{marginTop:20, flexDirection:'row'}}>
         <Button 
--- assets/components/ListCustomers.js
+++ assets/components/ListCustomers.js
@@ -1,10 +1,45 @@
-import { View, Text } from "react-native";
-import { styles } from '../styles/styles'
+import { View, Text, FlatList } from "react-native";
+import { Button } from "react-native-paper";
+import { styles } from '../styles/styles';
+import axios from "axios";
+import { useEffect, useState } from "react";
+
 
 export default function ListCustomers(){
+    const [data, setData] = useState([])
+
+    const getCustomers = async() => {
+        const response = await axios.get(`http://127.0.0.1:3000/api/clientes/`)
+        setData(response.data)
+    }
+
+    useEffect(()=>{
+      getCustomers();
+       //console.log(data);
+      
+    })
+    
     return(
-        <View style={styles.container}>
-            <Text>Aquí se mostraran los clientes</Text>
-        </View>
+             <View style={styles.container}>
+                <Text>Listado de Clientes</Text>
+
+                <Button
+                    style={{ backgroundColor: "gray", marginLeft: 10, marginBottom:10 }}
+                    icon="account-credit-card-outline"
+                    mode="contained"
+                    onPress={getCustomers}
+                    >
+                    Listar clientes     
+                </Button>
+                <FlatList
+                    data={data}
+                    renderItem={({item}) => 
+                    <Text style={{backgroundColor:'gray', borderRadius:10, padding:10, textAlign:'center',marginTop:5}}>
+                        {item.nombre} {item.apellidos}
+                    </Text>
+                    }      
+                />
+             </View>
+
     )
 }
